acb5903a410fb86a37d8f4700b2f7b46c7a77dbc
LTM0714 <80049651+LTM0714@users.noreply.github.com>
2025-08-11 16:11:29 +0900

    Delete myarea/src/main/java/com/myarea/myarea/repository/SublocationRepository.java

--- myarea/src/main/java/com/myarea/myarea/repository/SublocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.myarea.myarea.repository;
-
-import com.myarea.myarea.entity.SubLocation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubLocationRepository extends JpaRepository<SubLocation, Long> {
-}
-
