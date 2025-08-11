68046112342880d6b3cc9878d8ff67e25dccf150
LTM0714 <80049651+LTM0714@users.noreply.github.com>
2025-08-11 16:13:29 +0900

    Update and rename SublocationService.java to SubLocationService.java

--- myarea/src/main/java/com/myarea/myarea/service/SubLocationService.java
+++ myarea/src/main/java/com/myarea/myarea/service/SubLocationService.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class SublocationService {
+public class SubLocationService {
     @Autowired
     private SubLocationRepository sublocationRepository;
 
